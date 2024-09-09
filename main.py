0d224a4517d752f73f861e9316d693d4dbba7ecd
Trent-Duncan <duncantr131@northwested.org>
2024-09-09 14:21:33 -0400

    Update main.py
    updated code

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Hello world!')
+# Trent Duncan
+# 09 SEP 2024
+# Printing in Python
+
+first_name = "Trent"
+
+last_name = "Duncan"
+
+full_name = f"{first_name} {last_name}"
+
+message= f"Hello, {full_name}, would you like to learn some Python today?"
+
+print(message)
+
+quote_1= "bald is beutifull"
+
+famous_person1= "Mr wango"
+
+first_famous_quote= f" {famous_person1} once said {quote_1}"
+
+print(first_famous_quote)
+
+quote_2= "WAS THAT THE BITE OF 87?!"
+
+second_famous_person= "Markaplier"
+
+second_famous_quote= f" {second_famous_person} once yelled {quote_2}"
+
+print(second_famous_quote)
